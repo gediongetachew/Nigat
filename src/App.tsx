0b4c4f2d5f46481f41ccc1c460f987bd973cea5d
gideon <you@example.com>
2024-12-16 11:27:10 +0300

    navigation update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Link, Navigate, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import {
+  Link,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,12 +17,11 @@ import Footer from "./components/Footer";
 import { ROUTES } from "./util/data";
 import { useState } from "react";
 import Logo from "./assets/Logo.svg";
-import Logo2 from "./assets/Logo2.svg"
+import Logo2 from "./assets/Logo2.svg";
 import Menu0ne from "./assets/menuWhite.svg";
 import MenuTwo from "./assets/menuPrimary.svg";
 import LogoSmall from "./assets/LogoSmall.svg";
 import CallToActionBtn from "./components/callToActionBtn";
-
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
@@ -45,11 +51,20 @@ function App() {
   return (
     <article>
       <article className="absolute top-0 left-0 w-full bg-transparent flex items-center justify-start p-5 pr-10 font-trap-Regular font-[700] z-[2000]">
-  
-        <section className={`w-[20%] lg:w-[30%] ${(pathname === ROUTES[2] || pathname === ROUTES[4] )? "flex" : "hidden" }  lg:flex`}>
+        <section
+          className={`w-[20%] lg:w-[30%] ${
+            pathname === ROUTES[2] || pathname === ROUTES[4] ? "flex" : "hidden"
+          }  lg:flex`}
+        >
           <Link to={ROUTES[0]}>
-            {pathname === ROUTES[1] || pathname === ROUTES[2] || pathname === ROUTES[4]  ? (
-              <img src={Logo2} alt="logo" className="w-[81px] lg:w-[203px] h-[27px] lg:h-[65px] " />
+            {pathname === ROUTES[1] ||
+            pathname === ROUTES[2] ||
+            pathname === ROUTES[4] ? (
+              <img
+                src={Logo2}
+                alt="logo"
+                className="w-[81px] lg:w-[203px] h-[27px] lg:h-[65px] "
+              />
             ) : (
               <img
                 src={Logo}
@@ -60,77 +75,63 @@ function App() {
           </Link>
         </section>
         <section className="w-[80%] lg:w-[60%]  hidden lg:flex lg:justify-end ">
-        <ul className="pt-[2vh] w-full h-full   flex  justify-between  pl-[5%] text-left uppercase leading-[42px]">
+          <ul className="pt-[2vh] w-full h-full   flex  justify-between  pl-[5%] text-left uppercase leading-[42px]">
             <li className="text-[15px] xl:text-[16px] text-secondary-light hover:text-secondary font-semibold cursor-pointer flex flex-row items-center gap-2">
-              <button className={` bg-secondary rounded-full w-[16px] h-[16px] ${ pathname === ROUTES[0] ? 'flex' : "hidden" }`} />
-                
-              
-              <Link to={ROUTES[0]} >
-                Home
-              </Link>
+              <button
+                className={` bg-secondary rounded-full w-[16px] h-[16px] mb-[4px] ${
+                  pathname === ROUTES[0] ? "flex" : "hidden"
+                }`}
+              />
+
+              <Link to={ROUTES[0]}>Home</Link>
             </li>
             <li className="text-[15px] xl:text-[16px] text-secondary-light hover:text-secondary font-semibold cursor-pointer flex items-center gap-2">
-            <button className={` bg-secondary rounded-full w-[16px] h-[16px] ${ pathname === ROUTES[1] ? 'flex' : "hidden" }`} />
-              <Link to={ROUTES[1]} >
-                Who we are
-              </Link>
+              <button
+                className={` bg-secondary rounded-full w-[16px] h-[16px] mb-[4px] ${
+                  pathname === ROUTES[1] ? "flex" : "hidden"
+                }`}
+              />
+              <Link to={ROUTES[1]}>Who we are</Link>
             </li>
             <li className="text-[15px] xl:text-[16px] text-secondary-light hover:text-secondary font-semibold cursor-pointer flex items-center gap-2">
-            <button className={` bg-secondary rounded-full w-[16px] h-[16px] ${ pathname === ROUTES[2] ? 'flex' : "hidden" }`} />
-              <Link to={ROUTES[2]} >
-                Services
-              </Link>
+              <button
+                className={` bg-secondary rounded-full w-[16px] h-[16px] mb-[4px] ${
+                  pathname === ROUTES[2] ? "flex" : "hidden"
+                }`}
+              />
+              <Link to={ROUTES[2]}>Services</Link>
             </li>
             <li className="text-[15px] xl:text-[16px] text-secondary-light hover:text-secondary font-semibold cursor-pointer flex items-center gap-2">
-            <button className={` bg-secondary rounded-full w-[16px] h-[16px] ${ pathname === ROUTES[3] ? 'flex' : "hidden" }`} />
-              <Link to={ROUTES[3]} >
-                COntact us
-              </Link>
+              <button
+                className={` bg-secondary rounded-full w-[16px] h-[16px] mb-[4px] ${
+                  pathname === ROUTES[3] ? "flex" : "hidden"
+                }`}
+              />
+              <Link to={ROUTES[3]}>COntact us</Link>
             </li>
             <li className=" text-[15px] xl:text-[16px] flex justify-between items-center  text-secondary-light hover:text-secondary font-semibold cursor-pointer gap-2">
-            <button className={` bg-secondary rounded-full  w-[16px] h-[16px] ${ pathname === ROUTES[4] ? 'flex' : "hidden" }`} />
-              <Link to={ROUTES[4]} >
-              
-                Order Now
-              </Link>
-             
+              <button
+                className={` bg-secondary rounded-full  w-[16px] h-[16px] mb-[4px] ${
+                  pathname === ROUTES[4] ? "flex" : "hidden"
+                }`}
+              />
+              <Link to={ROUTES[4]}>Order Now</Link>
             </li>
-
-            
           </ul>
         </section>
- 
       </article>
-       <article
-        className="full-height-article absolute top-0 right-0 h-full flex"
-        
-      >
+      <article className="full-height-article absolute top-0 right-0 h-full flex">
         <Header
           showMenu={showMenu}
           setShowMenu={setShowMenu}
           showSideContent={showSideContent}
           setShowSideContent={setShowSideContent}
         />
-      </article> 
+      </article>
       <Routes>
-        <Route
-          path={ROUTES[0]}
-          element={
-            <Home  />
-          }
-        />
-        <Route
-          path={ROUTES[1]}
-          element={
-            <About  />
-          }
-        />
-        <Route
-          path={ROUTES[2]}
-          element={
-            <Services  />
-          }
-        />
+        <Route path={ROUTES[0]} element={<Home />} />
+        <Route path={ROUTES[1]} element={<About />} />
+        <Route path={ROUTES[2]} element={<Services />} />
         <Route path={ROUTES[3]} element={<ContactUs />} />
         <Route path={ROUTES[4]} element={<Order />} />
         <Route path="*" element={<Navigate to={ROUTES[0]} />} />
